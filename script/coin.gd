fc464567de320eb34d0b8310ab8e84e7be87912f
jayllfpt <phamphungoctraivl@gmail.com>
2025-12-31 23:16:52 +0700

    init source

---
+++ script/coin.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+
+class_name Interactable
+
+@export var prompt_message: String = "Nhặt đồ" # Chữ hiện lên màn hình
+
+@export var value: int = 1
+
+func interact(player):
+	player.add_coin(value) # Gọi hàm bên Player
+	queue_free()           # Xóa vật phẩm sau khi nhặt
